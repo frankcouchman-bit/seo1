aa5b0739894534ac4ae17c441ee0f6548f03a8ad
frankcouchman-bit <frankcouchman@gmail.com>
2025-10-15 19:44:29 +0200

    astro
    Added Vite alias configuration for easier imports.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import { fileURLToPath } from 'node:url'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-});
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
+})
